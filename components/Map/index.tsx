9c9dd61d3103dd2c92e36aba8d37d47356048632
강동욱 <ehddnr8563@gmail.com>
2025-04-21 14:41:38 +0900

    feat: 선택한 숙소 ui 구현 및 커스텀 마커 표시

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 /*global kakao*/
 
 import Script from 'next/script'
+import { SetStateAction } from 'react'
 
 declare global {
   interface Window {
@@ -16,7 +17,11 @@ const DEFAULT_LAT = 37.565337
 const DEFAULT_LNG = 126.9772095
 const ZOOM_LEVEL = 7
 
-export default function Map() {
+export default function Map({
+  setSelectedRoom,
+}: {
+  setSelectedRoom: React.Dispatch<SetStateAction<Room | null>>
+}) {
   const getRooms = async () => {
     const { data } = await axios('/api/rooms')
     return data as Room[]
@@ -42,6 +47,28 @@ export default function Map() {
       rooms?.map((room) => {
         // 마커 표시될 위치
         const markerPosition = new window.kakao.maps.LatLng(room.lat, room.lng)
+
+        // 마커 이미지 설정
+        const imageSrc = '/images/marker-icon.png'
+        const imageSize = new window.kakao.maps.Size(30, 30)
+        const imageOption = { offset: new window.kakao.maps.Point(16, 46) }
+
+        // 마커 이미지를 생성합니다
+        const markerImage = new window.kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption,
+        )
+
+        // 마커를 생성합니다
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage,
+        })
+
+        // 마커가 지도 위에 표시되도록 설정
+        marker.setMap(map)
+
         // custom overlay를 설정
         const content = `<div class="custom_overlay">${room.price?.toLocaleString()}원</div>`
 
@@ -53,6 +80,15 @@ export default function Map() {
 
         // 커스텀 오버레이가 지도 위에 표시되도록 설정
         customOverlay.setMap(map)
+
+        // 마커에 클릭 이벤트를 등록
+        window.kakao.maps.event.addListener(marker, 'click', function () {
+          setSelectedRoom(room)
+        })
+
+        window.kakao.maps.event.addListener(map, 'click', function () {
+          setSelectedRoom(null)
+        })
       })
     })
   }
