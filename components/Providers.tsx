0dc85ca06b9bc19dd6a75964b830e76debe160f0
강동욱 <ehddnr8563@gmail.com>
2025-04-21 12:25:37 +0900

    chore: 리액트 쿼리 설정

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+'use client'
 
-function Providers() {
-  return <div>Providers</div>
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const queryClient = new QueryClient()
+
+function Providers({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default Providers
