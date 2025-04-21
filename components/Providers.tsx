57fbf8c967d3d3934b6d977b1f56898727514a75
강동욱 <ehddnr8563@gmail.com>
2025-04-21 15:59:56 +0900

    feat: 상세페이지 공유하기 기능 추가

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
 
 interface Props {
   children: React.ReactNode
@@ -13,6 +14,7 @@ function Providers({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
--- components/RoomDetail/HeaderSection.tsx
+++ components/RoomDetail/HeaderSection.tsx
@@ -1,15 +1,15 @@
 'use client'
 
+import Image from 'next/image'
 import { useState } from 'react'
-import { Room } from '@/interface/room'
 
-import { CiShare1 } from 'react-icons/ci'
 import { CiHeart } from 'react-icons/ci'
 import { AiOutlineUnorderedList } from 'react-icons/ai'
 
-import Image from 'next/image'
+import { Room } from '@/interface/room'
 import { BLUR_DATA_URL } from '@/constants/placeholder'
 import ImageListModal from './ImageListModal'
+import ShareButton from './ShareButton'
 
 export default function HeaderSection({ data }: { data: Room }) {
   const [showImageModal, setShowImageModal] = useState<boolean>(false)
@@ -20,13 +20,7 @@ export default function HeaderSection({ data }: { data: Room }) {
       <div className="flex w-full justify-between items-center px-4">
         <div className="underline text-xs md:text-sm mt-2">{data.address}</div>
         <div className="flex gap-2 text-xs md:text-sm mt-2">
-          <button
-            type="button"
-            className="flex gap-2 items-center px-2 py-1.5 rounded-lg hover:bg-black/10"
-          >
-            <CiShare1 />
-            <span className="underline">공유하기</span>
-          </button>
+          <ShareButton data={data} />
           <button
             type="button"
             className="flex gap-2 items-center px-2 py-1.5 rounded-lg hover:bg-black/10"
