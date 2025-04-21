e8c92af5d3db069f32973264514c496421c924df
강동욱 <ehddnr8563@gmail.com>
2025-04-21 12:26:13 +0900

    feat: 숙소 리스트 데이터 페칭 리액트 쿼리 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+
 import CategoryList from '@/components/CategoryList'
+import Loader from '@/components/Loader'
 import { GridLayout } from '@/components/RoomList/GridLayout'
 import { RoomItem } from '@/components/RoomList/RoomItem'
 import { Room } from '@/interface/room'
 
-export default async function Home() {
-  const data: Room[] = await getRooms()
+export default function Home() {
+  const getRooms = async () => {
+    const res = await fetch('/api/rooms')
+    if (!res.ok) {
+      throw new Error('failed to fetch')
+    }
+    return res.json()
+  }
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['rooms'],
+    queryFn: () => getRooms(),
+  })
 
   return (
     <>
       <CategoryList />
-      <GridLayout>
-        {data?.map((room) => <RoomItem room={room} key={room.id} />)}
-      </GridLayout>
+      {isLoading ? (
+        <Loader className="mt-60 mb-40" />
+      ) : (
+        <GridLayout>
+          {data?.map((room: Room) => <RoomItem room={room} key={room.id} />)}
+        </GridLayout>
+      )}
     </>
   )
-}
-
-async function getRooms() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/rooms`, {
-    cache: 'force-cache', // SSG
-  })
-
-  if (!res.ok) {
-    throw new Error('failed to fetch')
-  }
-
-  return res.json()
 }
