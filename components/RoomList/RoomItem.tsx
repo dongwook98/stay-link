c53c6023885d9d263688f4e8ca5ece20da6f01f7
강동욱 <ehddnr8563@gmail.com>
2025-04-21 14:31:51 +0900

    perf: 이미지 최적화, 레이아웃 시프트

--- components/RoomList/RoomItem.tsx
+++ components/RoomList/RoomItem.tsx
@@ -1,24 +1,19 @@
+import { BLUR_DATA_URL } from '@/constants/placeholder'
 import { Room } from '@/interface/room'
-import { useState } from 'react'
+import Image from 'next/image'
 
 export function RoomItem({ room }: { room: Room }) {
-  const [isLoaded, setIsLoaded] = useState(false)
-
   return (
     <div key={room.id}>
-      {/* 이미지 컨테이너: 비율 고정 */}
-      <div className="relative w-full pb-[66.66%] overflow-hidden rounded-md bg-gray-100">
-        <img
-          src={room?.images?.[0]}
-          alt={room.title}
-          onLoad={() => setIsLoaded(true)}
-          className={`
-            absolute top-0 left-0 w-full h-full object-cover
-            transition-opacity duration-200
-            ${isLoaded ? 'opacity-100' : 'opacity-0'}
-          `}
-        />
-      </div>
+      <Image
+        src={room?.images?.[0]}
+        alt={room.title}
+        width={500}
+        height={500}
+        placeholder="blur"
+        blurDataURL={BLUR_DATA_URL}
+        className="rounded-md w-full h-auto object-fit"
+      />
 
       <div className="mt-2 font-semibold text-sm">{room.title}</div>
       <span className="text-xs px-2 py-1 rounded-full bg-black text-white mt-1">
---
+++ constants/placeholder.ts
@@ -0,0 +1,3 @@
+/**  @example - https://png-pixel.com/ */
+export const BLUR_DATA_URL =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOcNX9WPQAGAgJUl8IWQgAAAABJRU5ErkJggg=='
