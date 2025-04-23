52067acd509bceacd734debc77661942651d39b2
강동욱 <ehddnr8563@gmail.com>
2025-04-23 17:35:29 +0900

    perf: 검색창에 디바운싱 적용

--- components/Form/RoomSearchFilter.tsx
+++ components/Form/RoomSearchFilter.tsx
@@ -7,14 +7,32 @@ import { AiOutlineSearch } from 'react-icons/ai'
 export default function RoomSearchFilter() {
   const setKeyword = useSetAtom(searchValueState)
 
+  const debounce = (func: Function, delay: number) => {
+    let timerId: NodeJS.Timeout
+    return function (...args: any[]) {
+      clearTimeout(timerId)
+      timerId = setTimeout(() => {
+        func(...args)
+      }, delay)
+    }
+  }
+
+  const handleDebounceSearch = (value: string) => {
+    setKeyword({ keyword: value })
+  }
+
+  const debouncedSearch = debounce(handleDebounceSearch, 1000)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e?.target?.value)
+  }
+
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 mb-10">
       <div className="flex items-center justify-center w-full gap-2">
         <input
           type="search"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setKeyword({ keyword: e?.target?.value })
-          }}
+          onChange={handleInputChange}
           placeholder="숙소명 검색"
           className="w-full block p-3 text-sm border border-gray-300 rounded-lg outline-none focus:border-rose-500 text-gray-800"
         />
